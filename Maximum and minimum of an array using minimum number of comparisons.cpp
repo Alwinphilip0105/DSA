788b2f75909246c4b884283a3c37ce41400a5402
Alwin <78615326+Alwinphilip0105@users.noreply.github.com>
2023-07-07 14:32:43 +0530

    Update Maximum and minimum of an array using minimum number of comparisons.cpp

--- Maximum and minimum of an array using minimum number of comparisons.cpp
+++ Maximum and minimum of an array using minimum number of comparisons.cpp
@@ -7,9 +7,39 @@ Input: arr[] = {22, 14, 8, 17, 35, 3}
 Output:  Minimum element is: 3
               Maximum element is: 35
 */
-
+//method 1
 void main()
-{
+{int a[10];
   int min,max=0;
-  
+  for(int i= 0;i<n;i++)
+  {
+ cin>>a[i];
+  }
+ for(int i= 0;i<n;i++)
+  {
+ if (a[i]<min)
+ {min=a[i];}
+ else (a[i]>max)
+   {
+   max=a[i];
+   }
+  }
+ cout<<"min:"<<min;
+ cout<<"max:"<<max;
+}
+//method 2
+void main()
+{int a[10];
+ 
+ for(int i= 0;i<n;i++)
+  {
+ cin>>a[i];
+  }
+ int min=a[0],max=a[0];
+ for(int i= 0;i<n;i++)
+  {
+ if (a[i]<min)
+   min=a[i];
+ else   
+  }
 }
